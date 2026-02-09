e2824f952a16ffc1fc830bac01d87db94cbe0132
23349 <23349@burnside.school.nz>
2026-02-09 14:36:16 +1300

    initial commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     #this is my homepage
-    return "Hi there matey"
+    return "En esta noche la vida es completa"
 
 if __name__ == "__main__":
     app.run(debug=True)
