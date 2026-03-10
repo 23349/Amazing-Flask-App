cbbd19f2fa5dad59981a4d6670a3ca6f5a4532b9
23349 <23349@burnside.school.nz>
2026-03-11 09:43:38 +1300

    Changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # My amazing flak app
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, request
 import sqlite3
 
 DATABASE = "appdatabase.db"
@@ -8,6 +8,7 @@ DATABASE = "appdatabase.db"
 app = Flask(__name__)
 
 
+# IDK
 def get_db():
     db = getattr(g, "_database", None)
     if db is None:
@@ -26,29 +27,50 @@ def query_db(query, args=(), one=False):
     cur = get_db().execute(query, args)
     rv = cur.fetchall()
     cur.close()
-    return (rv[0] if rv else None) if one else rv       
+    return (rv[0] if rv else None) if one else rv
 
 
 @app.route('/')
 def home():
     # This is my homepage and it will include the id, name, maker and img
     # Ask em queries mate
-    sql = """SELECT Cars.CarID, Manufacturer.Name, Cars.CarName, Cars.ImgURL 
+    sql = """SELECT Cars.CarID, Manufacturer.Name, Cars.CarName, Cars.ImgURL
         FROM Cars
-        JOIN Manufacturer 
+        JOIN Manufacturer
         ON Manufacturer.ManufacturerID = Cars.ManufacturerID;"""
     results = query_db(sql)
-    return render_template("home.html", results=results)
+    return render_template("home.html", results=results, all_cars=results)
 
 
 @app.route('/cars/<int:id>')
 def cars(id):
     # A single car based on the int id
-    sql = """SELECT * FROM Cars JOIN Manufacturer 
+    sql = """SELECT * FROM Cars
+            JOIN Manufacturer
             ON Manufacturer.ManufacturerID = Cars.ManufacturerID
             WHERE Cars.CarID = ?; """
     result = query_db(sql, (id,), True)
     return render_template("car.html", car=result)
+
+@app.route('/cars_search', methods=["GET","POST"])
+def cars_search():
+    print(request.form.get("car"))
+    sql = """SELECT * FROM Cars
+            JOIN Manufacturer
+            ON Manufacturer.ManufacturerID = Cars.ManufacturerID"""
+    result = query_db(sql)
+    return render_template("homepage.html", car=result)
+
+
+@app.route('/manufacturer/<int:id>')
+def manufacturer(id):
+    # Filters out cars based on their manufacturer
+    sql = """SELECT * FROM Cars
+            JOIN Manufacturer
+            ON Manufacturer.ManufacturerID = Cars.ManufacturerID
+            WHERE Manufacturer.ManufacturerID = ?; """
+    results = query_db(sql, (id,), False)
+    return render_template("manufacturer.html", Scuttlefish=results)
 
 
 if __name__ == "__main__":
